7f7c80f9fa9b3776c4b3325a53dd863eae3b7579
Hermann Kraus <hermr2d2+git@gmail.com>
2016-11-07 23:37:24 +0100

    Do not require a fixed array for sensor messages.

--- sensor.h
+++ sensor.h
@@ -23,14 +23,12 @@ struct sensor_info_packet : public tiny_udp_packet
 #define sinfo(nr, type, size, scale, name) {nr, type, ((size) | ((scale) << 4)), 0, 0, name}
 #define cinfo(nr, type, size, scale, min, max, name) {(nr+0x40), type, ((size) | ((scale) << 4)), min, max, name}
 
-extern const sensor_info info_messages[];
-
-inline void send_sensor_info(uint8_t nr)
+inline void send_sensor_info_P(const sensor_info *msg)
 {
     sensor_info_packet p;
     p.flags = 1;
     p.size = sizeof(sensor_info);
-    memcpy_P(&p.info, &(info_messages[nr]), sizeof(sensor_info));
+    memcpy_P(&p.info, msg, sizeof(sensor_info));
     send_udp_packet(p, server_ip, port_sensor_data);
 }
 
